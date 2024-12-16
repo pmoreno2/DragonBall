356ad92d8978ec1a4f8e640dc8c143d253cb7b6f
Pedro Moreno Fernández <pmoreno@iesemilidarder.com>
2024-12-17 00:30:41 +0100

    v.1.10. CI/CD despliegue GitHub page

--- DragonBall/src/app/pages/character-detail-page/character-detail-page.component.spec.ts
+++ DragonBall/src/app/pages/character-detail-page/character-detail-page.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { CharacterDetailPageComponent } from './character-detail-page.component';
+import { DragonballService } from '../../services/dragonball.service';
 
 describe('CharacterDetailPageComponent', () => {
   let component: CharacterDetailPageComponent;
@@ -8,9 +10,9 @@ describe('CharacterDetailPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CharacterDetailPageComponent]
-    })
-    .compileComponents();
+      imports: [HttpClientTestingModule, RouterTestingModule, CharacterDetailPageComponent],
+      providers: [DragonballService]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CharacterDetailPageComponent);
     component = fixture.componentInstance;
