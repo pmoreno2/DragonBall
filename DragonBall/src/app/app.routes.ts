fb8368fda71821d7bf9d1c65cc72de748029f2dc
Pedro Moreno Fernández <pmoreno@iesemilidarder.com>
2024-12-17 00:23:50 +0100

    v1.9. Detalles de los personajes

--- DragonBall/src/app/app.routes.ts
+++ DragonBall/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CharacterListPageComponent } from './pages/character-list-page/character-list-page.component';
+import { CharacterDetailPageComponent } from './pages/character-detail-page/character-detail-page.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'personajes', component: CharacterListPageComponent },
+  { path: 'personajes/:id', component: CharacterDetailPageComponent },
   { path: '404', component: NotFoundComponent },
   { path: '**', redirectTo: '/404' } // Ruta comodín para manejar rutas no encontradas
 ];
